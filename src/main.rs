6de2c7012589e467fdf7c16951a2a3f50952b480
K <51281148+K-dash@users.noreply.github.com>
2024-11-02 13:51:01 +0900

    feat: Implement random operand generation for arithmetic quiz
    - `rand` クレートを依存関係として追加し、ランダムなオペランドの生成を実装
    - 加算と減算のクイズで、それぞれのオペレーションで異なるランダムな数値を使用
    - ユーザー入力のパースタイプを `i32` に統一し、プログラムの一貫性を向上

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,44 @@
+use rand::Rng;
+
 fn main() {
-    println!("1 + 1 = ??");
+    let op1 = rand::thread_rng().gen_range(1..100);
+    let op2 = rand::thread_rng().gen_range(1..100);
+
+    println!("{} + {} = ??", op1, op2);
     println!("?? の値を入力してください:");
     let mut ans_input = String::new(); // ユーザーからの回答を保持する変数
                                        // 標準入力から1行取得して、ans_input に代入する
     std::io::stdin().read_line(&mut ans_input).unwrap();
 
-    // ans_inputからtrimで改行を取り除き、parseで整数(u32)に変換する
-    let ans_input = ans_input.trim().parse::<u32>().unwrap();
+    // ans_inputからtrimで改行を取り除き、parseで整数(i32)に変換する
+    let ans_input = ans_input.trim().parse::<i32>().unwrap();
 
-    dbg!(ans_input); // 実行後に入力された値を確認
+    dbg!(ans_input);
 
-    if ans_input == 1 + 1 {
+    if ans_input == op1 + op2 {
         println!("正解!");
     } else {
         println!("不正解!");
     }
 
-    println!("1 - 4 = ??");
+
+    let op1 = rand::thread_rng().gen_range(1..100);
+    let op2 = rand::thread_rng().gen_range(1..100);
+
+    println!("{} - {} = ??", op1, op2);
     println!("?? の値を入力してください:");
     let mut ans_input = String::new(); // ユーザーからの回答を保持する変数
                                        // 標準入力から1行取得して、ans_input に代入する
     std::io::stdin().read_line(&mut ans_input).unwrap();
 
-    let ans_input = ans_input.trim().parse::<isize>().unwrap();
+    // ans_inputからtrimで改行を取り除き、parseで整数(i32)に変換する
+    let ans_input = ans_input.trim().parse::<i32>().unwrap();
 
-    dbg!(ans_input); // 実行後に入力された値を確認
+    dbg!(ans_input);
 
-    if ans_input == 1 - 4 {
+    if ans_input == op1 - op2 {
         println!("正解!");
     } else {
         println!("不正解!");
     }
-
-    println!("i32 が扱えるデータ範囲: {} ~ {}", isize::MIN, isize::MAX);
-    println!("u32 が扱えるデータ範囲: {} ~ {}", usize::MIN, usize::MAX);
 }
